ac0c69c5b8305a6902490264af6b69eb2bac12ed
Romeo Disca <romeo.disca@gmail.com>
2017-10-28 13:45:37 +0200

    feat: add endian and arch in blender file struct

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,24 @@ use std::io::Read;
 use std::vec::Vec;
 use std::str;
 
+#[derive(Debug)]
+pub enum Endian {
+  BigEndian,
+  LittleEndian,
+}
+
+#[derive(Debug)]
+pub enum Arch {
+  Arch32,
+  Arch64,
+}
 
 #[derive(Debug)]
 pub struct BlenderFile {
   pub file: String,
   pub version: String,
+  pub endian: Endian,
+  pub arch: Arch,
   pub content: Vec<u8>,
 }
 
@@ -22,16 +35,39 @@ impl BlenderFile {
 
     file.read_to_end(&mut vector).expect(&format!("File {:?} cannot be read.", file));
 
-    let out = BlenderFile { 
+    let arch = match *(&vector[7]) as char {
+        '-' => Arch::Arch64,
+        '_' => Arch::Arch32,
+        _ => panic!("architecture marker doesn't match"),
+      };
+
+    let endian = match *(&vector[8]) as char {
+        'v' => Endian::LittleEndian,
+        'V' => Endian::BigEndian,
+        _ => panic!("endian marker doesn't match"),
+      };
+
+    let out = BlenderFile {
       file: String::from(filename),
-      version: String::from(str::from_utf8(&vector[..12]).unwrap()),
+      version: String::from(str::from_utf8(&vector[9..12]).unwrap()),
       content: vector,
+      arch: arch,
+      endian: endian,
     };
 
 
     out
   }
 
+}
+
+macro_rules! matches{
+  ($e: expr, $p: pat) => (
+    match $e {
+      $p => true,
+      _ => false,
+    }
+  )
 }
 
 
@@ -44,7 +80,7 @@ mod tests {
   fn create_new_blenderfile() {
     let bf = BlenderFile::new("resources/rust-cube.blend");
     let result = bf.version;
-    let expected = String::from("BLENDER-v279");
+    let expected = String::from("279");
     assert_eq!(expected, result);
   }
 
@@ -52,6 +88,18 @@ mod tests {
   #[should_panic]
   fn create_new_blenderfile_by_unkonwn_file() {
     BlenderFile::new("resources/rust-cube.blend.none");
+  }
+
+  #[test]
+  fn verify_endian() {
+    let bf = BlenderFile::new("resources/rust-cube.blend");
+    assert!(matches!(bf.endian, Endian::LittleEndian));
+  }
+
+  #[test]
+  fn verify_architecture() {
+    let bf = BlenderFile::new("resources/rust-cube.blend");
+    assert!(matches!(bf.arch, Arch::Arch64));
   }
 
 }
